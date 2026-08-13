7ed2d73165813575c20f69e3759740f0ad2c998e
kentaro222 <magdracky@gmail.com>
2026-08-13 15:43:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,53 @@
-const CACHE_NAME = 'chill-walk-v9';
+const CACHE_NAME = 'chill-walk-v10';
+
 const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './player_1bit00_sheet.png'
-  './icon-192.png',
-  './icon-512.png'
+    './',
+    './index.html',
+    './manifest.json',
+    './player_1bit00_sheet.png',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
-// インストール時にキャッシュを保存
+// インストール時に必要なファイルをキャッシュ
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+
+    self.skipWaiting();
 });
 
 // 古いキャッシュを削除
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            );
         })
-      );
-    })
-  );
-  self.clients.claim();
+    );
+
+    self.clients.claim();
 });
 
-// オフライン時はキャッシュから返す (Cache First strategy)
+// GETリクエストのみキャッシュ優先で処理
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // キャッシュがあればそれを返し、なければネットワークへリクエスト
-        return response || fetch(event.request);
-      })
-  );
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+            })
+    );
 });
